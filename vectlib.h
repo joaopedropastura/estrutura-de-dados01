3811fe75a002483ac1c8ec44f21b0e072d389fad
Joao Pedro Soares <73509985+jpsoares01@users.noreply.github.com>
2023-03-28 08:47:02 -0300

    create new lib

---
+++ vectlib.h
@@ -0,0 +1,17 @@
+#ifndef VECTLIB_H
+
+#define VECTLIB_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+#define file1 "NomeRG10.txt"
+#define file2 "NomeRG1M.txt"
+#define file3 "NomeRG100M.txt"
+
+#endif
