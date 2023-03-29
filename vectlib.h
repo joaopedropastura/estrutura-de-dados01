d46617f1b2a0a19bbf98de03b62b9c68a5eeaeb0
Joao Pedro Soares <73509985+jpsoares01@users.noreply.github.com>
2023-03-29 10:54:30 -0300

    biblio pra ficar tudo mais suave

--- vectlib.h
+++ vectlib.h
@@ -14,4 +14,11 @@ using namespace std;
 #define file2 "NomeRG1M.txt"
 #define file3 "NomeRG100M.txt"
 
+void addVector(tuple<string, int> value, vector<tuple <string, int>> &temp);
+tuple <string, int> makeTuple (string line);
+vector<tuple <string, int>> readFile(string fileName);
+int menu();
+void createFile(string name, vector<tuple <string, int>> temp);
+
+
 #endif
