3033db5a0ea2f5ee49339a5a9d0469e81330d825
Joao Pedro Soares <73509985+jpsoares01@users.noreply.github.com>
2023-04-02 13:24:22 -0300

    salvando p mexer um pouco nisso aq

--- main.cpp
+++ main.cpp
@@ -1,61 +1,47 @@
 #include "vectlib.h"
 
-class Manipulate
+void resizeVector(vector<tuple<string, int>> &vetor, int flag = 1)
 {
-  private: 
-    vector<tuple <string, int>> result = {};
+  // vetor = (vector)realloc( vetor, sizeof(vector<tuple<string, int>>));
+}
 
-  public:
-    Manipulate(vector<tuple <string, int>> temp)
-    {
-      this -> result = temp;
-    }      
-    void insert(string value, int index)
-    {
-      tuple<string, int>nValue = makeTuple(value);
-      bool flag = false;
-      for (int i = 0; i < result.size() + 1; i++) 
-      {
-        
-        auto temp = result[i - 1];
-        if (i >= index)
-        {
-          if(i == index)
-          {
-            result.resize(result.size() + 1);
-            result[i] = nValue;
-            flag = true;
-          }
-          else
-           result[i] = flag ? temp : result[i];
-          // temp[i] = result[flag ? i-1 : i];
-        // i == index ? temp[i] = nValue : temp[i] = result[i] ;
-        }
-      }
-    }
-    vector<tuple <string, int>> getVect()
-    {
-      return result;
-    }
+class Manipulate {
+private:
+  vector<tuple<string, int>> result = {};
+
+public:
+  Manipulate(vector<tuple<string, int>> temp) { this->result = temp; }
+
+  void insert(string value, int index = 0)
+  {
+    tuple<string, int> nValue = makeTuple(value);
+    result.resize(result.size() + 1);
+    for (int i = result.size() - 1; i >= index; i--) 
+      result[i] = i != index ? result[i - 1] : nValue;
+  }
+
+  void remove(int index = 0)
+  {
+    for (int i = index; i <= index; i++) 
+      result[i] = result[i + 1];
+    result.resize(result.size() - 1);
+  }
+  vector<tuple<string, int>> getVect() { return result; }
 };
 
-// vector<t         le <string, int>> insert(vector<tuple <string, int>> temp, int index = 0)
-// {
-  
-// }
 int main() {
-
-  vector<tuple <string, int>> result = {};
+  int x = menu();
+  cout << x;
+  
+  auto start = high_resolution_clock::now(); 
+  vector<tuple<string, int>> result = {};
   result = readFile(file1);
   Manipulate m(result);
-  m.insert("joao paulo,10009943", 3);
-  int x  = menu();
-  cout << x;
+  m.insert("joao paulo,11009943", 1);
+  // m.remove(0);
   createFile("resultado", m.getVect());
-  // for (auto elem : result) 
-  // {
-  //   int num = get<1>(elem);
-  //   string str = get<0>(elem);
-  //   cout << str << "," << num << endl;
-  // }
+  auto end = high_resolution_clock::now();
+  duration<double> duration = end - start;
+  cout << "Tempo de execução: " << duration.count() << " segundos" << endl;
+
 }
