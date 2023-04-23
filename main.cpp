c212a134d46acd0f5ecce91392d1d62977d1b0c8
Joao Pedro Soares <73509985+jpsoares01@users.noreply.github.com>
2023-04-23 13:35:57 -0300

    passando do replit

--- main.cpp
+++ main.cpp
@@ -1,47 +1,122 @@
 #include "vectlib.h"
 
-void resizeVector(vector<tuple<string, int>> &vetor, int flag = 1)
+void checkTime()
 {
-  // vetor = (vector)realloc( vetor, sizeof(vector<tuple<string, int>>));
-}
-
-class Manipulate {
-private:
   vector<tuple<string, int>> result = {};
 
-public:
-  Manipulate(vector<tuple<string, int>> temp) { this->result = temp; }
+  auto start = high_resolution_clock::now();
 
-  void insert(string value, int index = 0)
-  {
-    tuple<string, int> nValue = makeTuple(value);
-    result.resize(result.size() + 1);
-    for (int i = result.size() - 1; i >= index; i--) 
-      result[i] = i != index ? result[i - 1] : nValue;
-  }
-
-  void remove(int index = 0)
-  {
-    for (int i = index; i <= index; i++) 
-      result[i] = result[i + 1];
-    result.resize(result.size() - 1);
-  }
-  vector<tuple<string, int>> getVect() { return result; }
-};
-
-int main() {
-  int x = menu();
-  cout << x;
-  
-  auto start = high_resolution_clock::now(); 
-  vector<tuple<string, int>> result = {};
-  result = readFile(file1);
-  Manipulate m(result);
-  m.insert("joao paulo,11009943", 1);
-  // m.remove(0);
-  createFile("resultado", m.getVect());
   auto end = high_resolution_clock::now();
   duration<double> duration = end - start;
   cout << "Tempo de execução: " << duration.count() << " segundos" << endl;
+}
 
+void menuFile()
+{
+  cout << "----- DIGITE O NUMERO DO ARQUIVO DESEJADO -----\n";
+  cout << "{00} - Para encerrar o programa " << '\n';
+}
+
+string chooseFile()
+{
+  
+}
+
+int main() {  
+  
+  vector<tuple<string, int>> result = {};
+  string file =  file1;
+  linkedList list;
+  string name;
+  int rg;
+  
+  result = readFile(file);
+  auto start = high_resolution_clock::now();
+  addLinkedList(result, &list);
+  auto end = high_resolution_clock::now();
+  duration<double> duration = end - start;
+  cout << "Tempo de execução (LISTA ENCADEADA)  : " << duration.count() << " segundos" << endl;
+  // start = high_resolution_clock::now();
+  ordenateList m(result);
+  // end = high_resolution_clock::now();
+  // duration = end - start;
+  // cout << "Tempo de execução (LISTA SEQUENCIAL) : " << duration.count() << " segundos" << endl;
+  switch (menu())
+  {
+    case 0:
+      break;
+    case 1:
+      cin >> rg;
+      start = high_resolution_clock::now();
+      getName(rg, file);
+      end = high_resolution_clock::now();
+      duration = end - start;
+      cout << "Tempo de execução: " << duration.count() << " segundos" << endl;
+      menu();
+    case 2:
+      cin >> name;
+      start = high_resolution_clock::now();
+      getValue(name, file);
+      end = high_resolution_clock::now();
+      duration = end - start;
+      cout << "Tempo de execução: " << duration.count() << " segundos" << endl;
+      menu();
+    case 3:
+      cin >> name;
+      start = high_resolution_clock::now();
+      m.insert(name, 0);
+      end = high_resolution_clock::now();
+      duration = end - start;
+      cout << "Tempo de execução: " << duration.count() << " segundos" << endl;
+      menu();
+    case 4:
+      cin >> name;
+      start = high_resolution_clock::now();
+      m.remove(0);
+      end = high_resolution_clock::now();
+      duration = end - start;
+      cout << "Tempo de execução: " << duration.count() << " segundos" << endl;
+      menu();
+    case 5:
+      cin >> name;
+      start = high_resolution_clock::now();
+      m.insert(name, -1);
+      end = high_resolution_clock::now();
+      duration = end - start;
+      cout << "Tempo de execução: " << duration.count() << " segundos" << endl;
+      menu();
+    case 6:
+      cin >> name;
+      start = high_resolution_clock::now();
+      m.remove(-1);
+      end = high_resolution_clock::now();
+      duration = end - start;
+      cout << "Tempo de execução: " << duration.count() << " segundos" << endl;
+      menu();
+     case 7:
+      cin >> name;
+      cin >> rg;
+      start = high_resolution_clock::now();
+      m.insert(name, rg);
+      end = high_resolution_clock::now();
+      duration = end - start;
+      cout << "Tempo de execução: " << duration.count() << " segundos" << endl;
+      menu();
+    case 8:
+      cin >> rg;
+      start = high_resolution_clock::now();
+      m.remove(rg);
+      end = high_resolution_clock::now();
+      duration = end - start;
+      cout << "Tempo de execução: " << duration.count() << " segundos" << endl;
+      menu();
+    case 9:
+      list.print();
+      menu();
+    
+  }
+  createFile("resultado", m.getVect());
+  end = high_resolution_clock::now();
+  duration = end - start;
+  cout << "Tempo de execução: " << duration.count() << " segundos" << endl;
 }
