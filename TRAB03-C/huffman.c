abaca33f2702e71aea6e4c703990094aaec34712
joaopedropastura <jpsoares111@hotmail.com>
2023-12-11 02:52:50 -0300

    ruffman step

---
+++ TRAB03-C/huffman.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct HuffmanNode {
+    char str[100];
+    unsigned freq;
+    struct HuffmanNode *left, *right;
+} HuffmanNode;
+
+typedef struct MinHeap {
+    unsigned size;
+    unsigned capacity;
+    HuffmanNode **array;
+} MinHeap;
+
+HuffmanNode* newNode(char str, unsigned freq) {
+    HuffmanNode* temp = (HuffmanNode*)malloc(sizeof(HuffmanNode));
+    temp->left = temp->right = NULL;
+    temp->str = str;
+    temp->freq = freq;
+    return temp;
+}
+
+MinHeap* createMinHeap(unsigned capacity) {
+    MinHeap* minHeap = (MinHeap*)malloc(sizeof(MinHeap));
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+    minHeap->array = (HuffmanNode**)malloc(minHeap->capacity * sizeof(HuffmanNode*));
+    return minHeap;
+}
+
+void swapHuffmanNode(HuffmanNode** a, HuffmanNode** b) {
+    HuffmanNode* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void minHeapify(MinHeap* minHeap, int idx) {
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq)
+        smallest = left;
+
+    if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq)
+        smallest = right;
+
+    if (smallest != idx) {
+        swapHuffmanNode(&minHeap->array[smallest], &minHeap->array[idx]);
+        minHeapify(minHeap, smallest);
+    }
+}
+
+int isSizeOne(MinHeap* minHeap) {
+    return (minHeap->size == 1);
+}
+
+HuffmanNode* extractMin(MinHeap* minHeap) {
+    HuffmanNode* temp = minHeap->array[0];
+    minHeap->array[0] = minHeap->array[minHeap->size - 1];
+    --minHeap->size;
+    minHeapify(minHeap, 0);
+    return temp;
+}
+
+void insertMinHeap(MinHeap* minHeap, HuffmanNode* huffmanNode) {
+    ++minHeap->size;
+    int i = minHeap->size - 1;
+    while (i && huffmanNode->freq < minHeap->array[(i - 1) / 2]->freq) {
+        minHeap->array[i] = minHeap->array[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+    minHeap->array[i] = huffmanNode;
+}
+
+void buildMinHeap(MinHeap* minHeap) {
+    int n = minHeap->size - 1;
+    int i;
+    for (i = (n - 1) / 2; i >= 0; --i)
+        minHeapify(minHeap, i);
+}
+
+void printArr(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; ++i)
+        printf("%d", arr[i]);
+    printf("\n");
+}
+
+int isLeaf(HuffmanNode* root) {
+    return !(root->left) && !(root->right);
+}
+
+MinHeap* createAndBuildMinHeap(char str[], int freq[], int size) {
+    MinHeap* minHeap = createMinHeap(size);
+    for (int i = 0; i < size; ++i)
+        minHeap->array[i] = newNode(str[i], freq[i]);
+    minHeap->size = size;
+    buildMinHeap(minHeap);
+    return minHeap;
+}
+
+HuffmanNode* buildHuffmanTree(char str[], int freq[], int size) {
+    HuffmanNode *left, *right, *top;
+    MinHeap* minHeap = createAndBuildMinHeap(str, freq, size);
+    while (!isSizeOne(minHeap)) {
+        left = extractMin(minHeap);
+        right = extractMin(minHeap);
+        top = newNode('$', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+        insertMinHeap(minHeap, top);
+    }
+    return extractMin(minHeap);
+}
+
+void printCodes(HuffmanNode* root, int arr[], int top) {
+    if (root->left) {
+        arr[top] = 0;
+        printCodes(root->left, arr, top + 1);
+    }
+    if (root->right) {
+        arr[top] = 1;
+        printCodes(root->right, arr, top + 1);
+    }
+    if (isLeaf(root)) {
+        printf("%c: ", root->str);
+        printArr(arr, top);
+    }
+}
+
+void HuffmanCodes(char str[], int freq[], int size) {
+    HuffmanNode* root = buildHuffmanTree(str, freq, size);
+    int arr[100], top = 0;
+    printCodes(root, arr, top);
+}
+
+int main() {
+    char arr[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+    int freq[] = {5, 9, 12, 13, 16, 45};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    HuffmanCodes(arr, freq, size);
+    return 0;
+}
